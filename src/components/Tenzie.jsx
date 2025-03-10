7a375eca610b3bc3db9ca37151261e536de3fde0
looonnng <122837733+looonnng@users.noreply.github.com>
2025-03-09 22:39:04 -0500

    feat: check if game is won

--- src/components/Tenzie.jsx
+++ src/components/Tenzie.jsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 
 export default function Tenzie() {
   const [dice, setDice] = useState(getAllNewDice());
+  const [isGameWon, setIsGameWon] = useState(false);
 
   const diceElements = dice.map((dieObj) => (
     <Die
@@ -16,6 +17,12 @@ export default function Tenzie() {
     />
   ));
 
+  function checkGameWon() {
+    setIsGameWon(
+      dice.every((die, index, arr) => die.value === arr[0]['value']),
+    );
+  }
+
   function rollDice() {
     setDice((prevDice) =>
       prevDice.map((die) =>
@@ -24,20 +31,27 @@ export default function Tenzie() {
     );
   }
 
+  function newGame() {
+    setDice(getAllNewDice());
+    setIsGameWon(false);
+  }
+
   function hold(id) {
     setDice((prevDice) =>
       prevDice.map((die) =>
         die.id === id ? { ...die, isHeld: !die.isHeld } : die,
       ),
     );
+
+    checkGameWon();
   }
 
   return (
     <div className="tenzies flex-center">
       <Info />
       <div className="dice-wrapper">{diceElements}</div>
-      <button onClick={rollDice} className="roll-btn">
-        Roll
+      <button onClick={isGameWon ? newGame : rollDice} className="roll-btn">
+        {isGameWon ? 'New Game' : 'Roll'}
       </button>
     </div>
   );
